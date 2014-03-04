cd09884d66decc2d54845efa54248b7d7a2a9059
Alexander Sapozhnikov <shoorick@mail.ru>
2014-03-05 04:35:07 +0600

    Transliteration can be performed by another methods

--- parse-xml-simple.pl
+++ parse-xml-simple.pl
@@ -5,31 +5,105 @@ use XML::Simple qw(:strict);
 use Data::Dumper;
 
 
-sub transliterate_to_german {
-    local $_ = shift;
-    s/Е/Je/g;
-    s/Ё/Jo/g;
-    s/Ж/Sch/g;
-    s/Х/Ch/g;
-    s/Ч/Tsch/g;
-    s/Ш/Sch/g;
-    s/Щ/Schtsch/g;
-    s/Ю/Ju/g;
-    s/Я/Ja/g;
-    s/е/je/g;
-    s/ё/jo/g;
-    s/ж/sch/g;
-    s/х/ch/g;
-    s/ч/tsch/g;
-    s/ш/sch/g;
-    s/щ/schtsch/g;
-    s/ю/ju/g;
-    s/я/ja/g;
-    s/[ъь]//g;
-    y/АБВГДЕЗИЙКЛМНОПРСТУФЦЫЭ/ABWGDESIJKLMNOPRSTUFZYE/;
-    y/абвгдезийклмнопрстуфцыэ/abwgdesijklmnoprstufzye/;
+sub transliterate {
+    my %subs = (
+        'de' => sub {
+            local $_ = shift;
+            s/Е/Je/g;
+            s/Ё/Jo/g;
+            s/Ж/Sch/g;
+            s/Х/Ch/g;
+            s/Ч/Tsch/g;
+            s/Ш/Sch/g;
+            s/Щ/Schtsch/g;
+            s/Ю/Ju/g;
+            s/Я/Ja/g;
+            s/е/je/g;
+            s/ё/jo/g;
+            s/ж/sch/g;
+            s/х/ch/g;
+            s/ч/tsch/g;
+            s/ш/sch/g;
+            s/щ/schtsch/g;
+            s/ю/ju/g;
+            s/я/ja/g;
+            s/[ъь]//g;
+            y[АБВГДЕЗИЙКЛМНОПРСТУФЦЫЭабвгдезийклмнопрстуфцыэ]
+             [ABWGDESIJKLMNOPRSTUFZYEabwgdesijklmnoprstufzye];
+            return $_;
+        },
+        'en' => sub {
+            local $_ = shift;
+            s/Е/Ye/g;
+            s/Ё/Yo/g;
+            s/Ж/Zh/g;
+            s/Х/Kh/g;
+            s/Ц/Ts/g;
+            s/Ч/Ch/g;
+            s/Ш/Sh/g;
+            s/Щ/Shch/g;
+            s/Ю/Yu/g;
+            s/Я/Ya/g;
+            s/е/ye/g;
+            s/ё/yo/g;
+            s/ж/zh/g;
+            s/х/kh/g;
+            s/ц/ts/g;
+            s/ч/ch/g;
+            s/ш/sh/g;
+            s/щ/shch/g;
+            s/ю/yu/g;
+            s/я/ya/g;
+            s/[ъь]//g;
+            y[АБВГДЕЗИЙКЛМНОПРСТУФЫЭабвгдезийклмнопрстуфыэ]
+             [ABVGDEZIYKLMNOPRSTUFYEabvgdeziyklmnoprstufye];
+            return $_;
+        },
+        'fr' => sub {
+            local $_ = shift;
+            s/А[ий]/Aï/g;
+            s/Е[ий]/Ieï/g;
+            s/Э[ий]/Eï/g;
+            s/О[ий]/Oï/g;
+            s/Е/Ie/g;
+            s/Ё/Io/g;
+            s/Ж/J/g;
+            s/У/Ou/g;
+            s/Х/Kh/g;
+            s/Ц/Ts/g;
+            s/Ч/Tch/g;
+            s/Ш/Ch/g;
+            s/Щ/Chtch/g;
+            s/Ю/Iu/g;
+            s/Я/Ia/g;
+            s/а[ий]/aï/g;
+            s/е[ий]/ieï/g;
+            s/э[ий]/eï/g;
+            s/о[ий]/oï/g;
+            s/е/ie/g;
+            s/ё/io/g;
+            s/ж/j/g;
+            s/у/ou/g;
+            s/х/kh/g;
+            s/ц/ts/g;
+            s/ч/tch/g;
+            s/ш/ch/g;
+            s/щ/chtch/g;
+            s/ю/iu/g;
+            s/я/ia/g;
+            s/[ъь]//g;
+            y[АБВГДЗИЙКЛМНОПРСТФЫЭабвгдзийклмнопрстфыэ]
+             [ABVGDZIYKLMNOPRSTFYEabvgdziyklmnoprstfye];
+            return $_;
+        },
+        'ru' => sub {
+            return shift; # do nothing
+        },
+    );
     
-    return $_;
+    my ( $what, $how ) = @_;
+    return $subs{$how}($what);
+    
 }
 
 
@@ -48,11 +122,11 @@ foreach my $osm ( @{ $structure->{'osm'} } ) {
         
         # Check existence and copy if possible
         if ( $tags->{'name'} ) {
-            $tags->{'name:ru'}->{'v'} //= $tags->{'name'}->{'v'};
-                #if $tags{'name'}->{'v'} =~ /[\x400-x4FF]/; # name contains cyrillic
-            $tags->{'name:de'}->{'v'} //= transliterate_to_german($tags->{'name'}->{'v'});
-            print "name:ru = ", $tags->{'name:ru'}->{'v'};
-            print "name:de = ", $tags->{'name:ru'}->{'v'};
+                #if $tags->{'name'}->{'v'} =~ /[\x400-\x4FF]/; # name contains cyrillic
+            foreach my $language ( qw( de en fr ru ) ) {
+                $tags->{"name:$language"}->{'v'} //= transliterate($tags->{'name'}->{'v'}, $language);
+            }
+            $tags->{"int_name"}->{'v'} //= $tags->{'name:en'}->{'v'};
         }
         
     }
